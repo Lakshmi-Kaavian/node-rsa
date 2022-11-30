9e55b5f223758681079e7cc6dbb5fefd63a88aea
Lakshmi <thangalakshmi.r@kaaviansys.com>
2022-11-30 15:35:06 +0530

    changed file path

--- generate.js
+++ generate.js
@@ -1,13 +1,14 @@
 const Nodersa= require('node-rsa');
 const fs= require('fs');
+const path = require('path');
 
 function Generatekey(){
     const key = new Nodersa().generateKeyPair();
     const public_key=key.exportKey('public');
     const private_key=key.exportKey('private');
-    fs.openSync("./keys/publickey.pem","w");
-    fs.writeFileSync("./keys/publickey.pem",public_key,'utf8');
-    fs.openSync("./keys/private.pem","w");
+    fs.openSync(path.join(__dirname +"/keys/publickey.pem","w"));
+    fs.writeFileSync(path.join(__dirname +"/keys/publickey.pem",public_key,'utf8'));
+    fs.openSync(path.join(__dirname + "/keys/private.pem"),"w");
     fs.writeFileSync("./keys/private.pem",private_key,'utf8');
     
 }
